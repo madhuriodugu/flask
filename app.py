df1249dd9644938d626255551acf748f66e771aa
Madhuri_odugu <55620983+madhuriodugu@users.noreply.github.com>
2020-09-14 16:14:44 +0530

    Update app.py

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ from resources.store import Store, StoreList
 
 app = Flask(__name__)
 
-app.confid['DEBUG'] = True
+app.config['DEBUG'] = True
 
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', 'sqlite:///data.db')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
